3b6b9d7e9da0e7f92b0e150cc791d8de7fef84dc
Sniedze <Sniedze.Sedola@sets.lv>
2024-05-21 11:47:53 +0300

    export model-info constructor

--- autoform-feature.d.ts
+++ autoform-feature.d.ts
@@ -1,6 +1,9 @@
 import { AutoForm as _AutoForm } from './autoform.js';
+import { ModelInfo as _ModelInfo } from './model-info.js';
+import type { AbstractModel, DetachedModelConstructor } from '@hilla/form';
 export * from './autoform.js';
 export * from './autoform-field.js';
 export * from './model-info.js';
+export declare const createModelInfo: (model: DetachedModelConstructor<AbstractModel>, idPropertyName?: string) => _ModelInfo;
 export declare const AutoForm: typeof _AutoForm;
 //# sourceMappingURL=autoform-feature.d.ts.map
--- autoform-feature.js
+++ autoform-feature.js
@@ -1,10 +1,13 @@
 import { AutoForm as _AutoForm } from "./autoform.js";
 import { featureRegistration } from "./util";
+import { ModelInfo as _ModelInfo } from "./model-info.js";
 export * from "./autoform.js";
 export * from "./autoform-field.js";
 export * from "./model-info.js";
+const createModelInfo = (model, idPropertyName) => new _ModelInfo(model, idPropertyName);
 const AutoForm = featureRegistration(_AutoForm, "AutoForm");
 export {
-  AutoForm
+  AutoForm,
+  createModelInfo
 };
 //# sourceMappingURL=autoform-feature.js.map
